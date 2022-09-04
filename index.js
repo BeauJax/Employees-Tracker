dc13eee08c6c24c3762e10df89721cf13d2e876d
BeauJax <beaujackson88@yahoo.com>
2022-09-04 16:11:36 -0400

    server has been added

---
+++ index.js
@@ -0,0 +1,13 @@
+const inquirer = require('inquirer');
+const db = require('./db/connection');
+require('console.table');
+
+//main menu
+function menu() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'options',
+            message: 'What would you like to do?',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit']
+        }
