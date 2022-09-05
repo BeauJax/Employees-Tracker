3f2110ba9da9f6191e7b735f8125e525474f7ff7
BeauJax <beaujackson88@yahoo.com>
2022-09-04 20:41:18 -0400

    added function

--- index.js
+++ index.js
@@ -8,6 +8,106 @@ function menu() {
         {
             type: 'list',
             name: 'options',
-            message: 'What would you like to do?',
-            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit']
+            message: 'Where would you like to go?',
+            choices: ['View departments list', 'View epmployee roles', 'View employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee position/Role', 'Exit']
         }
+    ])
+    .then((results) => {
+        let option = results.options;
+        if (option === 'View departments list') {
+            viewDepartments();
+        } else if (option === 'View employee roles') {
+            viewRoles();
+        } else if (option === 'View all employees') {
+            viewEmployees();
+        } else if (option === 'Add a department') {
+            addDepartment();
+        } else if (option === 'Add a role') {
+            addRole();
+        } else if (option === 'Add an employee') {
+            addEmployee();
+        } else if (option === 'Update an employee position') {
+            updateRole();
+        } else if (option === "View a department's budget") {
+            viewBudget();
+        }
+        else {
+            quitMenu();
+        }
+    })
+}
+
+//list of departments 
+function viewDepartments() {
+db.query('SELECT * FROM department', (err, result) => {
+    if (err) throw err
+    console.table(result);
+    menu();
+})
+}
+
+//lists of role 
+function viewRoles() {
+db.query('SELECT * FROM role', (err, result) => {
+    if (err) throw err
+    console.table(result);
+    menu();
+})
+}
+
+//list of employees
+function viewEmployees() {
+db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department_name FROM employee LEFT JOIN role ON employee.role_id=role.id LEFT JOIN department ON role.department_id=department.id', (err, result) => {
+    if (err) throw err
+    console.table(result);
+    menu();
+})
+}
+
+//add user input
+function addDepartment() {
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'newDepartment',
+        message: 'Please enter a department name.'
+    }
+])
+    .then((answer) =>
+        db.promise().query('INSERT INTO department (name) VALUES (?)', answer.newDepartment, (err, result) => {
+            if (err) throw err
+            console.table(result);
+        })).then(() => {
+            menu();
+        })
+}
+
+//add user input 
+function addRole() {
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'newRole',
+        message: 'What is the new role?'
+    },
+    {
+        type: 'input',
+        name: 'newSalary',
+        message: "What is the new role's salary?"
+    },
+    {
+        type: 'list',
+        name: 'whichDepartment',
+        message: 'What is the department id? 1=Engineering, 2=IT, 3=Legal, 4=Marketing',
+        choices: ['1', '2', '3', '4']
+    }
+])
+    .then((answers) =>
+        db.promise().query('INSERT INTO role (title, salary, department_id) VALUE (?, ?, ?)', [answers.newRole, answers.newSalary, answers.whichDepartment], (err, result) => {
+            if (err) throw err
+            console.table(result);
+        })).then(() => {
+            menu();
+        })
+}
+
